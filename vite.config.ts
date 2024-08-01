6eedd385dfead866187f9bb86779339f91927f0e
Abdul <malik301002@gmail.com>
2024-08-01 09:43:03 +0700

    chore(init): vexpense project via create-cloudflare CLI
    Initial commit of the project

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { URL, fileURLToPath } from 'node:url';
+
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
